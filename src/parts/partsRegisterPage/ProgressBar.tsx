84a009c28cbd014f324cf1e373ec004c850a0922
Gamecoder11 <tha075bct034@tcioe.edu.np>
2023-08-25 23:10:54 +0545

    Added functionality to Register page

--- src/parts/partsRegisterPage/ProgressBar.tsx
+++ src/parts/partsRegisterPage/ProgressBar.tsx
@@ -1,37 +1,17 @@
 
 type ProgressBarPorps = {
-    values: {
-        completeLevel1: boolean;
-        completeLevel2: boolean;
-        completeLevel3?: boolean;
-    }
+    currentLevel: number;
 }
 
-export default function ProgressBar({ values }: ProgressBarPorps) {
-    let step = 1
-    let percent = 100
-    let width = "w-1/12"
-    const { completeLevel1, completeLevel2, completeLevel3 } = values
-
-    if (completeLevel3) {
-        width = "w-full"
-    }
-    else if (completeLevel2) {
-        step = 3
-        percent = 33
-        width = "w-2/3"
-    }
-    else if (completeLevel1) {
-        step = 2
-        percent = 66
-        width = "w-1/3"
-    }
-
+export default function ProgressBar({ currentLevel }: ProgressBarPorps) {
+    const step = currentLevel + 1;
+    const percent = [100, 66, 33];
+    const width = currentLevel == 0 ? 'w-1/12' : `w-${currentLevel}/3`;
     return (
         <div className="grid grid-cols-10 w-full">
             <div className=" col-start-2 col-end-10 flex  flex-col text-white w-full ">
-                <span className="text-md">step {step}of 3</span>
-                <span className="text-xl">{completeLevel3 ? "Completed" : `${percent}% to complete`}</span>
+                <span className="text-md">step {step} of 3</span>
+                <span className="text-xl">{`${percent[currentLevel]}% to complete`}</span>
                 <div className="bg-white w-full h-6 rounded-s-md rounded-e-xl mt-2">
                     <div className={`bg-pink-400 ${width} h-6 rounded-s-md rounded-e-md`}></div>
                 </div>
