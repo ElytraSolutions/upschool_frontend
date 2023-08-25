02aace5362762f57ed26095c34a51e6884c4ffe6
Saroj Prasad Mainali <sarojprasadmainali@gmail.com>
2023-08-25 23:11:34 +0545

    Started Coursedetailed page

---
+++ src/pages/Courses/courseDetail.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+interface RouteParams {
+  id?: string;
+}
+
+function courseDetail() {
+  const { id } = useParams<RouteParams>();
+  return (
+    <>
+      <h1>courseDetail</h1>
+      <div>
+        <h2>Course Details</h2>
+        {id ? <p>Course ID: {id}</p> : <p>No Course ID found in the URL</p>}
+      </div>
+    </>
+  );
+}
+
+export default courseDetail;
