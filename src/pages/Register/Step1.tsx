c27b955825a17182def5e3aa5158116598c8da51
Suven-p <pandeysuven@gmail.com>
2023-08-26 09:49:11 +0545

    Fix jsx errors

--- src/pages/Register/Step1.tsx
+++ src/pages/Register/Step1.tsx
@@ -160,8 +160,8 @@ export default function Step1({ oldValues, submitHandler }: IStep1Props) {
                             >
                                 <span className="text-xl">Next</span>
                                 <span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
                                     </svg>
                                 </span>
                             </button>
--- src/pages/Register/Step2.tsx
+++ src/pages/Register/Step2.tsx
@@ -15,6 +15,7 @@ const registerSchema = yup.object().shape({
 
 interface IStep2Props {
     oldValues: Record<string, any>;
+    backHandler: (values: any) => void;
     submitHandler: (values: any, onSubmitProps: any) => Promise<void>;
 }
 export default function Step2({ oldValues, backHandler, submitHandler }: IStep2Props) {
@@ -124,13 +125,13 @@ export default function Step2({ oldValues, backHandler, submitHandler }: IStep2P
                                         xmlns="http://www.w3.org/2000/svg"
                                         fill="none"
                                         viewBox="0 0 24 24"
-                                        stroke-width="1.5"
+                                        strokeWidth="1.5"
                                         stroke="currentColor"
                                         className="w-4 h-4"
                                     >
                                         <path
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
                                             d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
                                         />
                                     </svg>
@@ -147,13 +148,13 @@ export default function Step2({ oldValues, backHandler, submitHandler }: IStep2P
                                         xmlns="http://www.w3.org/2000/svg"
                                         fill="none"
                                         viewBox="0 0 24 24"
-                                        stroke-width="1.5"
+                                        strokeWidth="1.5"
                                         stroke="currentColor"
                                         className="w-4 h-4"
                                     >
                                         <path
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
                                             d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
                                         />
                                     </svg>
