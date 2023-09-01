d2f1bd36a214d1878989af67e28688b3d266f155
Saroj Prasad Mainali <sarojprasadmainali@gmail.com>
2023-09-01 22:55:47 +0545

    Dashboard Updated

--- src/pages/User/DashBoard/Dashboard.tsx
+++ src/pages/User/DashBoard/Dashboard.tsx
@@ -1,4 +1,10 @@
 import React, { useState } from 'react';
+import DashboardContent from './DashboardContent';
+import MyCourses from './MyCourses';
+import MyBooks from './MyBooks';
+import MyDonations from './MyDonations';
+import Logout from './Logout';
+import Setting from './Setting';
 
 interface DashboardProps {}
 
@@ -15,27 +21,43 @@ const Dashboard: React.FC<DashboardProps> = () => {
             <div className="md:w-1/5 p-4 border text-[#cecece] xl:w-1/6">
                 <ul className="mt-4 flex flex-col gap-4">
                     <li
-                        className=" cursor-pointer p-2 border-2 border-white hover:text-[#242254] hover:border-[#242254] rounded-lg"
+                        className={`cursor-pointer p-2 border-2 border-white rounded-lg ${
+                            selectedOption === 'Dashboard'
+                                ? 'text-[#242254] border-[#242254]'
+                                : 'hover:text-[#242254] hover:border-[#242254]'
+                        }`}
                         onClick={() => handleOptionClick('Dashboard')}
                     >
                         {' '}
                         <i className="fa fa-dashboard"></i> Dashboard
                     </li>
                     <li
-                        className=" cursor-pointer p-2 border-2 border-white hover:text-[#242254] hover:border-[#242254] rounded-lg"
+                        className={`cursor-pointer p-2 border-2 border-white rounded-lg ${
+                            selectedOption === 'My Courses'
+                                ? 'text-[#242254] border-[#242254]'
+                                : 'hover:text-[#242254] hover:border-[#242254]'
+                        }`}
                         onClick={() => handleOptionClick('My Courses')}
                     >
                         {' '}
                         <i className="fa fa-desktop"></i> My Courses
                     </li>
                     <li
-                        className=" cursor-pointer p-2 border-2 border-white hover:text-[#242254] hover:border-[#242254] rounded-lg"
+                        className={`cursor-pointer p-2 border-2 border-white rounded-lg ${
+                            selectedOption === 'My Books'
+                                ? 'text-[#242254] border-[#242254]'
+                                : 'hover:text-[#242254] hover:border-[#242254]'
+                        }`}
                         onClick={() => handleOptionClick('My Books')}
                     >
                         <i className="fa fa-book"></i> My Books
                     </li>
                     <li
-                        className=" cursor-pointer p-2 border-2 border-white hover:text-[#242254] hover:border-[#242254] rounded-lg"
+                        className={`cursor-pointer p-2 border-2 border-white rounded-lg ${
+                            selectedOption === 'My Donations & Purchase'
+                                ? 'text-[#242254] border-[#242254]'
+                                : 'hover:text-[#242254] hover:border-[#242254]'
+                        }`}
                         onClick={() =>
                             handleOptionClick('My Donations & Purchase')
                         }
@@ -43,13 +65,21 @@ const Dashboard: React.FC<DashboardProps> = () => {
                         <i className="fa fa-table"></i> My Donations & Purchase
                     </li>
                     <li
-                        className=" cursor-pointer p-2 border-2 border-white hover:text-[#242254] hover:border-[#242254] rounded-lg"
+                        className={`cursor-pointer p-2 border-2 border-white rounded-lg ${
+                            selectedOption === 'Setting'
+                                ? 'text-[#242254] border-[#242254]'
+                                : 'hover:text-[#242254] hover:border-[#242254]'
+                        }`}
                         onClick={() => handleOptionClick('Setting')}
                     >
                         <i className="fa fa-gear"></i> Setting
                     </li>
                     <li
-                        className=" cursor-pointer p-2 border-2 border-white hover:text-[#242254] hover:border-[#242254] rounded-lg"
+                        className={`cursor-pointer p-2 border-2 border-white rounded-lg ${
+                            selectedOption === 'Logout'
+                                ? 'text-[#242254] border-[#242254]'
+                                : 'hover:text-[#242254] hover:border-[#242254]'
+                        }`}
                         onClick={() => handleOptionClick('Logout')}
                     >
                         <i className="fa fa-power-off"></i> Logout
@@ -59,26 +89,15 @@ const Dashboard: React.FC<DashboardProps> = () => {
 
             {/* Content */}
             <div className="w-4/5 lg:w-5/6 p-4 bg-[#cecece]">
-                <h1 className="text-2xl">{selectedOption}</h1>
                 {/* Content specific to the selected option */}
-                {selectedOption === 'Dashboard' && (
-                    <p>Dashboard content goes here.</p>
-                )}
-                {selectedOption === 'My Courses' && (
-                    <p>My Courses content goes here.</p>
-                )}
-                {selectedOption === 'My Books' && (
-                    <p>My Books content goes here.</p>
-                )}
+                {selectedOption === 'Dashboard' && <DashboardContent />}
+                {selectedOption === 'My Courses' && <MyCourses />}
+                {selectedOption === 'My Books' && <MyBooks />}
                 {selectedOption === 'My Donations & Purchase' && (
-                    <p>My Donations & Purchase content goes here.</p>
+                    <MyDonations />
                 )}
-                {selectedOption === 'Setting' && (
-                    <p>Setting content goes here.</p>
-                )}
-                {selectedOption === 'Logout' && (
-                    <p>Logout content goes here.</p>
-                )}
+                {selectedOption === 'Setting' && <Setting />}
+                {selectedOption === 'Logout' && <Logout />}
             </div>
         </div>
     );
