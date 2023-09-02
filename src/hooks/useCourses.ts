3aff35c2066ff950b04e6d77b756f9359c7f20ba
Suven-p <pandeysuven@gmail.com>
2023-09-02 15:39:26 +0545

    Update routes according to backend

--- src/hooks/useCourses.ts
+++ src/hooks/useCourses.ts
@@ -10,7 +10,7 @@ function useCourses(category: string | null = null) {
     useEffect(() => {
         (async () => {
             try {
-                const res = await axiosInstance.get('/api/courses');
+                const res = await axiosInstance.get('/data/courses');
                 setCourses(res.data.data);
             } catch (error) {
                 console.log(error);
