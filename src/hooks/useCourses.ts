f78f531e78ecfcb8d9a254d98dc5755d637f9818
Suven-p <pandeysuven@gmail.com>
2023-09-02 16:50:33 +0545

    Handle in-transit behaviour for course

--- src/hooks/useCourses.ts
+++ src/hooks/useCourses.ts
@@ -18,6 +18,7 @@ function useCourses(category: string | null = null) {
         })();
     }, []);
 
+    if (!categories.length) return [];
     if (category) {
         const id = categories.find((c) => c.name === category)?.id;
         return courses.filter((c) => c.course_category_id === id);
