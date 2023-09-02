468cf65044c32e05f58d5da560dbb6f715eaf798
Suven-p <pandeysuven@gmail.com>
2023-09-02 22:50:51 +0545

    Fix eslint

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -37,7 +37,7 @@ export default function Navbar() {
         return () => {
             window.removeEventListener('scroll', handleScroll);
         };
-    }, []);
+    }, [refresh]);
 
     return (
         <>
--- src/pages/Logout/Logout.tsx
+++ src/pages/Logout/Logout.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';
 import axiosInstance from '../../config/Axios';
 import useUser from '../../hooks/useUser';
