8dfaa6181b9aea8f6dc95be1349fcdd4c74196a2
Gamecoder11 <tha075bct034@tcioe.edu.np>
2023-09-08 12:56:04 +0545

    Commented unused parts

--- src/pages/UploadBook/UploadBookStep5.tsx
+++ src/pages/UploadBook/UploadBookStep5.tsx
@@ -1,18 +1,22 @@
-interface IStep5Props {
-    oldValues: Record<string, any>;
-    submitHandler: (values: any, onSubmitProps: any) => Promise<void>;
-    backHandler: (values: any) => void;
-}
-const UploadBookStep5 = ({
-    oldValues,
-    submitHandler,
-    backHandler,
-}: IStep5Props) => {
-    return (
-        <>
-            <h1>uplodbookStep5</h1>
-        </>
-    );
-};
+// interface IStep5Props {
+//     oldValues: Record<string, any>;
+//     submitHandler: (values: any, onSubmitProps: any) => Promise<void>;
+//     backHandler: (values: any) => void;
+// }
+// const UploadBookStep5 = ({
+//     oldValues,
+//     submitHandler,
+//     backHandler,
+// }: IStep5Props) => {
+//     return (
+//         <>
+//             <h1>uplodbookStep5</h1>
+//         </>
+//     );
+// };
 
-export { UploadBookStep5 };
+// export { UploadBookStep5 };
+
+export default function UploadBookStep5() {
+    return <h1>hello world</h1>;
+}
