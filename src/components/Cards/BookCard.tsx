5cd12f8ddb61b5b815e90d2375ea53b19d2c70d5
Gamecoder11 <tha075bct034@tcioe.edu.np>
2023-09-10 21:32:51 +0545

    Designed BoorkCard component

--- src/components/Cards/BookCard.tsx
+++ src/components/Cards/BookCard.tsx
@@ -1,17 +1,30 @@
-export const BookCard = () => {
+type BookCardProps = {
+    book: {
+        id: number;
+        image: string;
+        writer: string;
+        title: string;
+        country: string;
+    };
+};
+export const BookCard = ({ book }: BookCardProps) => {
     return (
-        <div className=" rounded-lg text-sm lg:text-base grid  grid-rows-5 overflow-hidden bg-white max-h-fit border border-gray-300  w-80">
+        <div className=" rounded-lg text-sm lg:text-base overflow-hidden bg-white border border-gray-300  max-w-[270px] max-h-[355px] ">
             <img
-                className="row-span-3 w-full h-full"
-                src="https://source.unsplash.com/WEv55byHNkk"
+                className="w-full max-h-[190px]"
+                src={book.image}
                 alt="logo"
                 loading="lazy"
             />
 
-            <div className="row-span-2 flex flex-col gap-1 m-4">
-                <div className="block text-gray-500  font-light">Susannah</div>
-                <div className="block text-lg lg:text-xl">Imagine</div>
-                <div className="block text-gray-500 font-light">Australia</div>
+            <div className="flex flex-col gap-1 m-4 max-h-[164] w-full">
+                <div className="block text-gray-500  font-light">
+                    {book.writer}
+                </div>
+                <div className="block text-lg lg:text-xl">{book.title}</div>
+                <div className="block text-gray-500 font-light">
+                    {book.country}
+                </div>
                 <button
                     type="button"
                     className="p-3 my-1 bg-red-upschool text-white text-xs md:text-sm hover:cursor-pointer w-fit"
--- src/pages/Library/Library.tsx
+++ src/pages/Library/Library.tsx
@@ -1,24 +1,13 @@
 import { BookCard } from '../../components/Cards/BookCard';
+import { books } from '../../data/LibraryBooks';
 
 export default function Library() {
     return (
         <>
-            <div className="grid grid-cols-4 gap-6">
-                <BookCard />
-                <BookCard />
-                <BookCard />
-                <BookCard />
-                <BookCard />
-                <BookCard />
-                <BookCard />
-                <BookCard />
-                <BookCard />
-                <BookCard />
-                <BookCard />
-                <BookCard />
-                <BookCard />
-                <BookCard />
-                <BookCard />
+            <div className="grid  gap-1 xs:grid-cols-2 xs:gap-2 justify-items-center sm:grid-cols-3 sm:gap-3 lg:grid-cols-4 lg:gap-4">
+                {books.map((book, index) => (
+                    <BookCard key={index} book={book} />
+                ))}
             </div>
         </>
     );
