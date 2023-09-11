6d017b87ec0f303089b23a213d89c20652d4576e
Gamecoder11 <tha075bct034@tcioe.edu.np>
2023-09-11 18:16:31 +0545

    Designing searchBar

--- src/pages/Library/Library.tsx
+++ src/pages/Library/Library.tsx
@@ -20,27 +20,6 @@ export default function Library() {
         }));
     };
     console.log(searchParameters);
-    // const searchResultByCategory = (searchParameters.category &&
-    //     books.filter(
-    //         (book) => book.categories?.includes(searchParameters.category),
-    //     )) as Array<{
-    //     id: number;
-    //     image: string;
-    //     writer: string;
-    //     title: string;
-    //     country: string;
-    //     categories: string[];
-    // }>;
-    // console.log(searchResultByCategory);
-    // const searchResult =
-    //     searchResultByCategory.length > 0 &&
-    //     searchParameters.search.length > 0 &&
-    //     searchResultByCategory.filter((book) =>
-    //         book.title
-    //             .toLowerCase()
-    //             .includes(searchParameters.search.toLowerCase()),
-    //     );
-    // console.log(searchResult);
     const result = () => {
         if (searchParameters.category.length > 0) {
             const searchResultByCategory = (searchParameters.category &&
@@ -61,8 +40,9 @@ export default function Library() {
                         .toLowerCase()
                         .includes(searchParameters.search.toLowerCase()),
                 );
+            } else {
+                return searchResultByCategory;
             }
-            return searchResultByCategory;
         } else {
             if (searchParameters.search.length > 0) {
                 return books.filter((book) =>
@@ -71,22 +51,32 @@ export default function Library() {
                         .includes(searchParameters.search.toLowerCase()),
                 );
             }
-            return [];
+            return books;
         }
     };
 
     const searchResult = result();
     console.log(searchResult);
+    const BookNotFound = () => {
+        return (
+            <div className=" text-theme-color font-normal text-xs xs:text-base sm:text-lg lg:text-xl">
+                <span>
+                    No books meet your search criteria at this time. Please
+                    modify your search.
+                </span>
+            </div>
+        );
+    };
 
     return (
         <>
             <div className="flex flex-row justify-center">
-                <div className=" lg:w-5/6 xl:w-2/3 flex flex-col gap-6">
+                <div className="w-[95%] lg:w-5/6 xl:w-2/3 flex flex-col gap-6">
                     <div className=" w-full py-1 px-2 xs:px-5 sm:px-6 lg:px-7 ">
-                        <div className="flex flex-row border border-black items-center">
+                        <div className="flex flex-col  sm:flex-row border border-black items-center">
                             <input
                                 id="search"
-                                className=" focus:outline-none flex-1 p-4 pl-8"
+                                className=" focus:outline-none flex-1 p-4 md:pl-8  text-xs xs:text-sm md:text-base w-[95%] xs:w-fit sm:w-full "
                                 type="text"
                                 placeholder="Search book by tiltle..."
                                 onChange={handleChange}
@@ -94,13 +84,12 @@ export default function Library() {
                                 value={searchParameters.search}
                             ></input>
                             <select
-                                className=" focus:outline-none w-fit"
+                                className=" focus:outline-none text-xs xs:text-sm  md:text-base w-[95%]  xs:w-fit sm:w-[40%] md:w-fit"
                                 id="category"
                                 value={searchParameters.category}
                                 onChange={handleChange}
                                 name="category"
                             >
-                                {' '}
                                 <option value="">Filter by category</option>
                                 {categories.map((category, index) => (
                                     <option key={index} value={category}>
@@ -108,17 +97,21 @@ export default function Library() {
                                     </option>
                                 ))}
                             </select>
-                            <button>Search</button>
+                            <button className="px-6 py-2 text-xs xs:text-sm  md:text-base">
+                                Search
+                            </button>
                         </div>
                     </div>
                     <div className="grid w-full py-1 px-2 xs:px-5 sm:px-6 lg:px-7 gap-4 xs:grid-cols-2 xs:gap-3 justify-items-center sm:grid-cols-3 sm:gap-3 lg:grid-cols-4 lg:gap-4">
-                        {searchResult.length > 0
-                            ? searchResult.map((book, index) => (
-                                  <BookCard key={index} book={book} />
-                              ))
-                            : books.map((book, index) => (
-                                  <BookCard key={index} book={book} />
-                              ))}
+                        {searchResult.length > 0 ? (
+                            searchResult.map((book, index) => (
+                                <BookCard key={index} book={book} />
+                            ))
+                        ) : (
+                            <div className="xs:col-span-full mt-10 mb-20 h-[40vh]">
+                                <BookNotFound />
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
