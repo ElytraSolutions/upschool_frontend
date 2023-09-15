ef5b44d16225362e1130a4731ec1fb32f04512ec
Gamecoder11 <tha075bct034@tcioe.edu.np>
2023-09-15 22:22:21 +0545

    Initiated design of Settings Section

--- src/pages/User/DashBoard/Setting.tsx
+++ src/pages/User/DashBoard/Setting.tsx
@@ -1,5 +1,29 @@
+import SettingBoardBox from '../../../parts/UserDashboard/Boxes/SettingBoardBox';
+import TeacherIcon from '../../../assets/TeacherIcon.png';
+import Active from '../../../assets/Active.png';
+import AddIcon from '../../../assets/AddIcon.png';
 function Setting() {
-    return <div>Setting</div>;
+    return (
+        <div>
+            <div className="flex flex-col gap-8 w-full text-theme-color">
+                <div className="flex flex-row w-full gap-3 justify-start h-full">
+                    <SettingBoardBox
+                        icon={TeacherIcon}
+                        text={['Teacher Account', '', 'TeacherIcon']}
+                    />
+                    <SettingBoardBox
+                        icon={Active}
+                        text={['', 'students', 'Active']}
+                        number={295} // TODO fetch number of students from backend
+                    />
+                    <SettingBoardBox
+                        icon={AddIcon}
+                        text={['', 'Add student(s)', 'AddIcon']}
+                    />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Setting;
---
+++ src/parts/UserDashboard/Boxes/SettingBoardBox.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+type BoardBoxProps = {
+    icon: string;
+    text: string[];
+    number?: number;
+};
+const SettingBoardBox: React.FC<BoardBoxProps> = ({
+    icon,
+    text,
+    number,
+}): React.JSX.Element => {
+    return (
+        <>
+            <div className="border rounded-md border-theme-color px-0 md:px-4 w-60 text-theme-color flex flex-row justify-center items-center">
+                <div className="flex flex-col justify-center items-center gap-2 py-4 px-1">
+                    <img
+                        src={icon}
+                        alt="logo "
+                        width={`${text[2] === 'AddIcon' ? '40px' : '60px'}`}
+                        height={`${text[2] === 'AddIcon' ? '40px' : '60px'}`}
+                    />
+                    {number ? (
+                        <h1 className="text-3xl text-center">{number}</h1>
+                    ) : (
+                        text[0] && (
+                            <h1 className="text-2xl text-center ">{text[0]}</h1>
+                        )
+                    )}
+                    {text[1] && (
+                        <h1 className="text-center text-lg">{text[1]}</h1>
+                    )}
+                </div>
+            </div>
+        </>
+    );
+};
+export default SettingBoardBox;
