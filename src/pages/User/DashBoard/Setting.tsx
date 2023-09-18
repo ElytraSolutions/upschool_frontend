0908cefb249820ae692023933ba7ee95d605fcdc
Gamecoder11 <tha075bct034@tcioe.edu.np>
2023-09-18 11:28:42 +0545

    Initiating design of settings for teacher account

--- src/pages/User/DashBoard/Setting.tsx
+++ src/pages/User/DashBoard/Setting.tsx
@@ -2,7 +2,10 @@ import SettingBoardBox from '../../../parts/UserDashboard/Boxes/SettingBoardBox'
 import TeacherIcon from '../../../assets/TeacherIcon.png';
 import Active from '../../../assets/Active.png';
 import AddIcon from '../../../assets/AddIcon.png';
+
+import { useState } from 'react';
 function Setting() {
+    const [selectedOption, setSelectedOption] = useState<string>('My Profile');
     return (
         <div>
             <div className="flex flex-col gap-8 w-full text-theme-color">
@@ -20,6 +23,64 @@ function Setting() {
                         icon={AddIcon}
                         text={['', 'Add student(s)', 'AddIcon']}
                     />
+                </div>
+                {/* options */}
+                <div className="flex flex-row justify-start w-full gap-4 text-lg md:text-xl lg:text-2xl">
+                    {/* My Profile */}
+                    <div className="flex flex-col w-fit  gap-2">
+                        <div
+                            className={`${
+                                selectedOption === 'My Profile'
+                                    ? 'text-theme-color'
+                                    : 'text-gray-400'
+                            } hover:cursor-pointer w-full text-center px-1 sm:px-4 md:px-6 lg:px-10`}
+                            onClick={() => setSelectedOption('My Profile')}
+                        >
+                            My Profile
+                        </div>
+                        <div
+                            className={`w-full h-1 ${
+                                selectedOption === 'My Profile'
+                                    ? 'bg-theme-color'
+                                    : 'bg-gray-400'
+                            }`}
+                        ></div>
+                    </div>
+                    {/*Students */}
+                    <div className="flex flex-col w-fit gap-2 ">
+                        <div
+                            className={`${
+                                selectedOption === 'Students'
+                                    ? 'text-theme-color'
+                                    : 'text-gray-400'
+                            } hover:cursor-pointer text-center w-full px-1 sm:px-4 md:px-6 lg:px-10`}
+                            onClick={() => setSelectedOption('Students')}
+                        >
+                            {/* TODO fetch number form backend */}
+                            Students (3)
+                        </div>
+                        <div
+                            className={`w-full h-1 ${
+                                selectedOption === 'Students'
+                                    ? 'bg-theme-color'
+                                    : 'bg-gray-400'
+                            }`}
+                        ></div>
+                    </div>
+                </div>
+                {/* Detail section */}
+                <div className="w-full xl:w-10/12">
+                    {/* My Profile section */}
+                    {selectedOption === 'My Profile' && (
+                        <div className="">My Profile</div>
+                    )}
+                    {/* Students section */}
+                    {selectedOption === 'Students' && (
+                        <div className=" ">
+                            {/* TODO fetch Students data from backend and use it inside this componet */}
+                            Students
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
