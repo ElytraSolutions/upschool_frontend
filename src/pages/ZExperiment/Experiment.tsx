b6384b717efdfa97ef542fd0b856be7704734617
Gamecoder11 <tha075bct034@tcioe.edu.np>
2023-10-17 20:35:59 +0545

    Commented data

--- src/pages/ZExperiment/Experiment.tsx
+++ src/pages/ZExperiment/Experiment.tsx
@@ -4,36 +4,36 @@ import './experimet.css';
 import { NavLink } from 'react-router-dom';
 import useCourses from '../../hooks/useCourses';
 
-const CourseInfo = [
-    {
-        name: 'build-a-library-and-change-the-world',
-        image: 'https://upschool.co/wp-content/uploads/2023/05/library-2.png',
-    },
-    {
-        name: 'be-the-change',
-        image: 'https://upschool.co/wp-content/uploads/2023/05/be-the-change-1-1.png',
-    },
-    {
-        name: 'write-a-book',
-        image: 'https://upschool.co/wp-content/uploads/2023/05/Write-a-Book-1.png',
-    },
-    {
-        name: 'the-solar-system-and-beyond',
-        image: 'https://upschool.co/wp-content/uploads/2023/05/Solar-System-Homepage-Tile.png',
-    },
-    {
-        name: 'values-for-a-better-tommorrow',
-        image: 'https://upschool.co/wp-content/uploads/2023/05/values-1.png',
-    },
-    {
-        name: 'it-starts-with-you',
-        image: 'https://upschool.co/wp-content/uploads/2023/05/It-Starts-With-You-2.png',
-    },
-    {
-        name: 'the-power-of-one',
-        image: 'https://upschool.co/wp-content/uploads/2023/05/Power-of-One-Homepage-Tile.png',
-    },
-];
+// const CourseInfo = [
+//     {
+//         name: 'build-a-library-and-change-the-world',
+//         image: 'https://upschool.co/wp-content/uploads/2023/05/library-2.png',
+//     },
+//     {
+//         name: 'be-the-change',
+//         image: 'https://upschool.co/wp-content/uploads/2023/05/be-the-change-1-1.png',
+//     },
+//     {
+//         name: 'write-a-book',
+//         image: 'https://upschool.co/wp-content/uploads/2023/05/Write-a-Book-1.png',
+//     },
+//     {
+//         name: 'the-solar-system-and-beyond',
+//         image: 'https://upschool.co/wp-content/uploads/2023/05/Solar-System-Homepage-Tile.png',
+//     },
+//     {
+//         name: 'values-for-a-better-tommorrow',
+//         image: 'https://upschool.co/wp-content/uploads/2023/05/values-1.png',
+//     },
+//     {
+//         name: 'it-starts-with-you',
+//         image: 'https://upschool.co/wp-content/uploads/2023/05/It-Starts-With-You-2.png',
+//     },
+//     {
+//         name: 'the-power-of-one',
+//         image: 'https://upschool.co/wp-content/uploads/2023/05/Power-of-One-Homepage-Tile.png',
+//     },
+// ];
 
 const Experiment = () => {
     const courses = useCourses();
