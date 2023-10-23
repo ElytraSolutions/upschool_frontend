3a119be63dc5d1bfb5278cf5b1efaffc56d0a685
anuj_raymajhi <87628435+anuj-raymajhi@users.noreply.github.com>
2023-10-23 06:26:02 +0545

    Added project card for charity page

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,16 +1,16 @@
-import { useEffect, useState } from 'react';
-import axiosInstance from '../config/Axios';
-import useUser from '../hooks/useUser';
+// import { useEffect } from 'react';
+// import axiosInstance from '../config/Axios';
+// import useUser from '../hooks/useUser';
 import CharityProjectCard from './Charity/CharityProjectCard';
 
 function Test() {
-    const [data, setData] = useState(null);
-    const { user } = useUser();
-    useEffect(() => {
-        axiosInstance.get('/data/user').then((resp) => {
-            setData(resp.data);
-        });
-    }, []);
+    // const [data, setData] = useState(null);
+    // const { user } = useUser();
+    // useEffect(() => {
+    //     axiosInstance.get('/data/user').then((resp) => {
+    //         // setData(resp.data);
+    //     });
+    // }, []);
     return (
         <div>
             {/* {JSON.stringify(data)}
