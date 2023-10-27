79e312185c98326a72296121e50f0e151d53e6bf
Kaustuv Karki <karkikaustuv@gmail.com>
2023-10-27 23:16:50 +0545

    few corrections made

--- src/pages/GlobalReach/Map.tsx
+++ src/pages/GlobalReach/Map.tsx
@@ -42,7 +42,7 @@ const Map = ({ mouseCoordinates }) => {
         setLocId(null);
     };
 
-    const getLocationClassName = (location, index) => {
+    const getLocationClassName = () => {
         // console.log(location)
     };
 
