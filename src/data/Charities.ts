d471ecc06902112012e9cdea1b0473c000a0794a
ayushregmi <aregmi29@gmail.com>
2023-10-28 20:21:44 +0545

    project page

--- src/data/Charities.ts
+++ src/data/Charities.ts
@@ -1,12 +1,52 @@
 export const Charities = [
-    {name: "SO THEY CAN", logo: "https://upschool.co/wp-content/uploads/2022/06/so-they-can.png", url: 'https://upschool.co/charity/so-they-can/'},
-    {name: "THE JANE GOODALL INSTITUTE AUSTRALIA", logo: "https://upschool.co/wp-content/uploads/2022/06/Jane-Goodall-Institute-Australia.png",  url: 'https://upschool.co/charity/so-they-can/'},
-    {name: "SO THEY CAN", logo: "https://upschool.co/wp-content/uploads/2022/06/so-they-can.png",  url: 'https://upschool.co/charity/so-they-can/'},
-    {name: "THE JANE GOODALL INSTITUTE AUSTRALIA", logo: "https://upschool.co/wp-content/uploads/2022/06/Jane-Goodall-Institute-Australia.png",  url: 'https://upschool.co/charity/so-they-can/'},
-    {name: "SO THEY CAN", logo: "https://upschool.co/wp-content/uploads/2022/06/so-they-can.png",  url: 'https://upschool.co/charity/so-they-can/'},
-    {name: "THE JANE GOODALL INSTITUTE AUSTRALIA", logo: "https://upschool.co/wp-content/uploads/2022/06/Jane-Goodall-Institute-Australia.png",  url: 'https://upschool.co/charity/so-they-can/'},
-    {name: "SO THEY CAN", logo: "https://upschool.co/wp-content/uploads/2022/06/so-they-can.png",  url: 'https://upschool.co/charity/so-they-can/',},
-    {name: "THE JANE GOODALL INSTITUTE AUSTRALIA", logo: "https://upschool.co/wp-content/uploads/2022/06/Jane-Goodall-Institute-Australia.png",  url: 'https://upschool.co/charity/so-they-can/'},
-    {name: "SO THEY CAN", logo: "https://upschool.co/wp-content/uploads/2022/06/so-they-can.png",  url: 'https://upschool.co/charity/so-they-can/'}, 
-    {name: "THE JANE GOODALL INSTITUTE AUSTRALIA", logo: "https://upschool.co/wp-content/uploads/2022/06/Jane-Goodall-Institute-Australia.png",  url: 'https://upschool.co/charity/so-they-can/'},
+    {
+        name: 'SO THEY CAN',
+        logo: 'https://upschool.co/wp-content/uploads/2022/06/so-they-can.png',
+        url: 'https://upschool.co/charity/so-they-can/',
+    },
+    {
+        name: 'THE JANE GOODALL INSTITUTE AUSTRALIA',
+        logo: 'https://upschool.co/wp-content/uploads/2022/06/Jane-Goodall-Institute-Australia.png',
+        url: 'https://upschool.co/charity/so-they-can/',
+    },
+    {
+        name: 'SO THEY CAN',
+        logo: 'https://upschool.co/wp-content/uploads/2022/06/so-they-can.png',
+        url: 'https://upschool.co/charity/so-they-can/',
+    },
+    {
+        name: 'THE JANE GOODALL INSTITUTE AUSTRALIA',
+        logo: 'https://upschool.co/wp-content/uploads/2022/06/Jane-Goodall-Institute-Australia.png',
+        url: 'https://upschool.co/charity/so-they-can/',
+    },
+    {
+        name: 'SO THEY CAN',
+        logo: 'https://upschool.co/wp-content/uploads/2022/06/so-they-can.png',
+        url: 'https://upschool.co/charity/so-they-can/',
+    },
+    {
+        name: 'THE JANE GOODALL INSTITUTE AUSTRALIA',
+        logo: 'https://upschool.co/wp-content/uploads/2022/06/Jane-Goodall-Institute-Australia.png',
+        url: 'https://upschool.co/charity/so-they-can/',
+    },
+    {
+        name: 'SO THEY CAN',
+        logo: 'https://upschool.co/wp-content/uploads/2022/06/so-they-can.png',
+        url: 'https://upschool.co/charity/so-they-can/',
+    },
+    {
+        name: 'THE JANE GOODALL INSTITUTE AUSTRALIA',
+        logo: 'https://upschool.co/wp-content/uploads/2022/06/Jane-Goodall-Institute-Australia.png',
+        url: 'https://upschool.co/charity/so-they-can/',
+    },
+    {
+        name: 'SO THEY CAN',
+        logo: 'https://upschool.co/wp-content/uploads/2022/06/so-they-can.png',
+        url: 'https://upschool.co/charity/so-they-can/',
+    },
+    {
+        name: 'THE JANE GOODALL INSTITUTE AUSTRALIA',
+        logo: 'https://upschool.co/wp-content/uploads/2022/06/Jane-Goodall-Institute-Australia.png',
+        url: 'https://upschool.co/charity/so-they-can/',
+    },
 ];
