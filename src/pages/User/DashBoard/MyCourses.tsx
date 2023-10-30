fef13e212ef2fac6bcc6d33f3651dc2ce4f8bb7a
Gamecoder11 <tha075bct034@tcioe.edu.np>
2023-10-30 11:29:18 +0545

    Deleted ActiveCoursesComponent

--- src/pages/User/DashBoard/MyCourses.tsx
+++ src/pages/User/DashBoard/MyCourses.tsx
@@ -43,35 +43,12 @@ const courses = [
 ];
 function MyCourses() {
     const [selectedOption, setSelectedOption] =
-        useState<string>('Active Courses');
+        useState<string>('Enrolled Courses');
     return (
         <div className="h-full overflow-auto">
             <div className="sm:p-4 w-full ">
                 <div className="flex flex-col gap-10 w-full">
                     <div className=" grid grid-cols-3 justify-items-center xs:flex xs:flex-row xs:justify-center md:justify-start text-sm sm:text-base lg:text-lg w-full gap-0 xs:gap-2  sm:gap-4">
-                        {/* Active courses option */}
-                        <div className="flex flex-col w-fit gap-2">
-                            <div
-                                className={`${
-                                    selectedOption === 'Active Courses'
-                                        ? 'text-theme-color'
-                                        : 'text-gray-400'
-                                } hover:cursor-pointer w-full h-full text-center px-1 sm:px-4 md:px-6 lg:px-10 font-lexend`}
-                                onClick={() =>
-                                    setSelectedOption('Active Courses')
-                                }
-                            >
-                                {/* TODO fetch number form backend */}
-                                Active Courses (2)
-                            </div>
-                            <div
-                                className={`w-full h-1.5 ${
-                                    selectedOption === 'Active Courses'
-                                        ? 'bg-theme-color'
-                                        : 'bg-gray-400'
-                                }`}
-                            ></div>
-                        </div>
                         {/* Enrolled courses option */}
                         <div className="flex flex-col w-fit gap-2">
                             <div
@@ -121,15 +98,6 @@ function MyCourses() {
                     </div>
                     {/* Detail section */}
                     <div className="w-full">
-                        {/* Active courses section */}
-                        {selectedOption === 'Active Courses' && (
-                            <div className="grid xm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-6 md:gap-2 lg:gap-4 w-full ">
-                                {/* TODO fetch active courses data from backend and use it */}
-                                {courses.map((course, index) => (
-                                    <CourseBox key={index} detail={course} />
-                                ))}
-                            </div>
-                        )}
                         {/* Enrolled courses section */}
                         {selectedOption === 'Enrolled Courses' && (
                             <div className="grid xm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-6 md:gap-2 lg:gap-4 w-full ">
