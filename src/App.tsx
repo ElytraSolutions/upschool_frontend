3615f05540feef6fa8406db3299441742828f5b9
Gamecoder11 <tha075bct034@tcioe.edu.np>
2023-10-31 22:03:09 +0545

    Started Desining Challenges

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,7 @@ import Challenges from './pages/Challenges/Challenges';
 import CanvaPage from './pages/Canva/CanvaPage';
 import BulkRegistrationPage from './pages/BulkRegistration/BulkRegistrationPage';
 import Live from './pages/Live/Live';
+import WriteALetterAndChangeTheWorld from './pages/Challenges/WriteALetterAndChangeTheWorld/WriteALetterAndChangeTheWorld';
 
 const router = createBrowserRouter([
     {
@@ -180,6 +181,10 @@ const router = createBrowserRouter([
             {
                 path: '/challenges',
                 element: <Challenges />,
+            },
+            {
+                path: '/challenges/write-a-letter-and-change-the-world-with-upschool',
+                element: <WriteALetterAndChangeTheWorld />,
             },
             {
                 path: '/canva/',
