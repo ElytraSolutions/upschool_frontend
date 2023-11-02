ed3f459d4f6d00d5491d2ebea5dcff171858623d
anuj_raymajhi <87628435+anuj-raymajhi@users.noreply.github.com>
2023-11-02 18:00:57 +0545

    Bookpage: added popup

--- src/pages/BookPage/AuthorsRecommendedProject.tsx
+++ src/pages/BookPage/AuthorsRecommendedProject.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import projectImage from '../../assets/projectimage.jpg';
 import BooksPopUp from './BooksPopUp';
+import HelpPopup from './HelpPopup';
 
 const AuthorsRecommendedProject = () => {
     // replace with the project details
@@ -12,54 +13,73 @@ const AuthorsRecommendedProject = () => {
     // const projectLink = '';
     const generatedAmount = 'AU $20.0';
     const [showAuthorBookPopUp, setShowAuthorBookPopUp] = useState(false);
+    const [isHelpPopupOpen, setHelpPopupOpen] = useState(false);
+
+    const openHelpPopup = () => {
+        setHelpPopupOpen(true);
+    };
+
+    const closeHelpPopup = () => {
+        setHelpPopupOpen(false);
+    };
+
+    useEffect(() => {
+        document.body.style.overflow = isHelpPopupOpen ? 'hidden' : 'unset';
+    }, [isHelpPopupOpen]);
 
     return (
-        <div className="md:mb-10">
-            <div className="bg-gray-100 rounded-xl p-4 px-4">
-                <div className="font-bold text-xl border-b pb-2 border-gray-300">
-                    <h1>Author's Recommended Project</h1>
-                </div>
-                <div className="mt-2 text-sm">
-                    <p>{projectDescription}</p>
+        <>
+            <div className="md:mb-10">
+                <div className="bg-gray-100 rounded-xl p-4 px-4">
+                    <div className="font-bold text-xl border-b pb-2 border-gray-300">
+                        <h1>Author's Recommended Project</h1>
+                    </div>
+                    <div className="mt-2 text-sm">
+                        <p>{projectDescription}</p>
+                    </div>
+                    <br />
+                    <img src={projectImg} alt="project image"></img>
+                    <br />
+                    <div className="font-black text-xl underline">
+                        <a className="hover:cursor-default">{projectName}</a>
+                    </div>
+                    <div className="mt-2 text-sm">
+                        <p>{projectProvider}</p>
+                    </div>
+                    <br />
+                    <div>
+                        <button
+                            className="text-blue-950 text-sm underline font-semibold"
+                            onClick={openHelpPopup}
+                        >
+                            Learn how you can help
+                        </button>
+                    </div>
                 </div>
                 <br />
-                <img src={projectImg} alt="project image"></img>
-                <br />
-                <div className="font-black text-xl underline">
-                    <a className="hover:cursor-default">{projectName}</a>
+                <div className="bg-gray-100 rounded-xl p-4 px-4">
+                    <div>Total amount this has generated for Charity:</div>
+                    <div className="text-pink-700 text-2xl font-bold text-right">
+                        {generatedAmount}
+                    </div>
                 </div>
-                <div className="mt-2 text-sm">
-                    <p>{projectProvider}</p>
+                <div>
+                    <button
+                        className="border border-gray-900 w-full h-full py-3 mt-4 rounded-xl font-bold"
+                        onClick={() => {
+                            setShowAuthorBookPopUp(true);
+                        }}
+                    >
+                        View all books from this author
+                    </button>
                 </div>
-                <br />
-                <div className="text-blue-950 text-sm underline font-semibold">
-                    <a className="hover:cursor-pointer">
-                        Learn how you can help
-                    </a>
-                </div>
+                <BooksPopUp
+                    showAuthorBookPopUp={showAuthorBookPopUp}
+                    setShowAuthorBookPopUp={setShowAuthorBookPopUp}
+                />
             </div>
-            <br />
-            <div className="bg-gray-100 rounded-xl p-4 px-4">
-                <div>Total amount this has generated for Charity:</div>
-                <div className="text-pink-700 text-2xl font-bold text-right">
-                    {generatedAmount}
-                </div>
-            </div>
-            <div>
-                <button
-                    className="border border-gray-900 w-full h-full py-3 mt-4 rounded-xl font-bold"
-                    onClick={() => {
-                        setShowAuthorBookPopUp(true);
-                    }}
-                >
-                    View all books from this author
-                </button>
-            </div>
-            <BooksPopUp
-                showAuthorBookPopUp={showAuthorBookPopUp}
-                setShowAuthorBookPopUp={setShowAuthorBookPopUp}
-            />
-        </div>
+            {isHelpPopupOpen && <HelpPopup onClose={closeHelpPopup} />}
+        </>
     );
 };
 
