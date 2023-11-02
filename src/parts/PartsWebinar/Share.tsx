188d65d8a3220708b635ca89512959be6db9c164
bikrantbdr <bikrantbdr@gmail.com>
2023-11-02 09:16:39 +0545

    team-main section error solved and share buttons in webinar page added

--- src/parts/PartsWebinar/Share.tsx
+++ src/parts/PartsWebinar/Share.tsx
@@ -1,12 +1,22 @@
 import resolveImgURL from '../../utlis/resolveImgURL';
+import {
+    // EmailShareButton,
+    // EmailIcon,
+    FacebookShareButton,
+    FacebookIcon,
+    LinkedinShareButton,
+    LinkedinIcon,
+    TwitterShareButton,
+    TwitterIcon,
+} from 'react-share';
 
 resolveImgURL;
-const Share = () => {
+const Share = ({ event }) => {
     return (
-        <div className=" w-[80%] lg:w-[35%] flex pt-4">
+        <div className=" w-[80%] lg:w-[35%] flex flex-col gap-4 pt-4">
             {/* share div */}
             <div className="w-full h-[25vh] rounded-xl overflow-hidden">
-                <div className=" w-full h-[25vh] flex flex-col justify-center items-center gap-4  relative  hover:scale-110 origin-center transition-all">
+                <div className=" w-full h-[25vh] flex flex-col justify-center items-center gap-4  relative  hover:scale-110 origin-center ease-in-out duration-700">
                     <div className="text-[1.5rem] text-white font-bold ">
                         Register for this event
                     </div>
@@ -25,6 +35,67 @@ const Share = () => {
                         alt=""
                         className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
                     />
+                </div>
+            </div>
+            {/* presentor div */}
+            <div className="w-full h-[20vh] rounded-xl border-2 overflow-hidden flex flex-col gap-3 justify-center items-center bg-gradient-to-b from-gray-300 to-white-500 hover:from- hover:bg-gradient-to-t">
+                <div className="w-[45px] h-[45px] rounded-full border-[0.25px] p-3 border-theme-color">
+                    <img
+                        src={resolveImgURL('images/webinar/teacher-icon.svg')}
+                        alt=""
+                        className="w-full h-full object-contain"
+                    />
+                </div>
+                <div className="text-[1.1rem] text-theme-color font-bold">
+                    {event.mentor}
+                </div>
+                <div className="text-[1rem] text-theme-color">Presenter</div>
+            </div>
+            {/* share */}
+            <div className="w-full h-[20vh] flex flex-col gap-2 justify-center items-center">
+                <div className="flex gap-4 items-center">
+                    <img
+                        src={resolveImgURL('images/webinar/share-icon.svg')}
+                        alt=""
+                        className="w-[25px] h-[30px]"
+                    />
+                    <div className="text-[1.1rem] text-theme-color font-bold">
+                        Share this event
+                    </div>
+                </div>
+                {/* social medias */}
+                <div className="flex gap-4">
+                    <FacebookShareButton
+                        url={`https://upschool.co${window.location.pathname}`}
+                        quote="test quote"
+                        hashtag="#upschool"
+                    >
+                        <FacebookIcon size={32} round={true} />
+                    </FacebookShareButton>
+                    <TwitterShareButton
+                        url={`https://upschool.co${window.location.pathname}`}
+                        title={event.title}
+                        hashtags={['upschool']}
+                    >
+                        <TwitterIcon size={32} round={true} />
+                    </TwitterShareButton>
+                    <LinkedinShareButton
+                        url={`https://www.linkedin.com/shareArticle?mini=true&url=https://upschool.co${window.location.pathname}`}
+                        title={event.title}
+                        summary={event.description}
+                        source="upschool.co"
+                    >
+                        <LinkedinIcon size={32} round={true} />
+                    </LinkedinShareButton>
+                    {/* <EmailShareButton
+                        onClick={() => console.log('email')}
+                        openShareDialogOnClick
+                        url={`https://upschool.co${window.location.pathname}`}
+                        subject={event.title}
+                        body={event.description}
+                    >
+                        <EmailIcon size={32} round={true} />
+                    </EmailShareButton> */}
                 </div>
             </div>
         </div>
