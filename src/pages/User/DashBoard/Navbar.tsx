737fff214ff03ef6f5b6b19cfdae593f1e726e2e
Gamecoder11 <tha075bct034@tcioe.edu.np>
2023-11-05 13:25:17 +0545

    Edited UserDashboardNavbar

--- src/pages/User/DashBoard/Navbar.tsx
+++ src/pages/User/DashBoard/Navbar.tsx
@@ -3,6 +3,7 @@ import useScreenWidthAndHeight from '../../../hooks/useScreenWidthAndHeight';
 import { NavLink } from 'react-router-dom';
 import resolveImgURL from '../../../utlis/resolveImgURL';
 import useUser from '../../../hooks/useUser';
+import { ComputerMenu } from '../../../parts/PartsNavbar/LargeScreenMenuBar';
 
 type NavbarProps = {
     setIsopen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -11,7 +12,7 @@ type NavbarProps = {
 function Navbar({ setIsopen }: NavbarProps) {
     const { user } = useUser();
     const [isScrolled, setIsScrolled] = useState(false);
-    const { isSmallHeight } = useScreenWidthAndHeight();
+    const { isTabScreen, isSmallHeight } = useScreenWidthAndHeight();
 
     useEffect(() => {
         const handleScroll = () => {
@@ -33,23 +34,27 @@ function Navbar({ setIsopen }: NavbarProps) {
             <div
                 className={`${
                     isScrolled && 'fixed'
-                } z-10 flex justify-between px-1 py-2 sm:p-4 border items-center ${
+                } z-10 flex items-center justify-between bg-theme-color px-1 py-2 sm:p-4 border ${
                     isSmallHeight ? 'h-[15dvh]' : 'h-[10dvh]'
-                }  w-full bg-white gap-2`}
+                }  w-full  gap-2`}
             >
-                <div className=" max-w-xs">
-                    <NavLink to="/">
-                        <img
-                            src={resolveImgURL('/images/logo.png')}
-                            className="h-10 w-full"
-                            alt=""
-                        />
-                    </NavLink>
+                <div className="flex flex-row items-center gap-5 ">
+                    <div className=" max-w-xs">
+                        <NavLink to="/">
+                            <img
+                                src={resolveImgURL('/images/logo.png')}
+                                className="h-10 w-full"
+                                alt=""
+                            />
+                        </NavLink>
+                    </div>
+                    {isTabScreen && <ComputerMenu />}
                 </div>
+
                 <div className="flex items-center gap-2 sm:gap-4">
-                    <div className="flex justify-center items-center h-8 w-8 sm:w-10 sm:h-10 overflow-hidden bg-gray-400 rounded-full dark:bg-gray-600">
+                    <div className="flex justify-center items-center h-8 w-8 sm:w-10 sm:h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
                         <svg //imported from tailwind components templates user
-                            className="w-7 h-7 sm:w-9 sm:h-9 text-white "
+                            className="w-7 h-7 sm:w-9 sm:h-9 text-gray-400 "
                             fill="currentColor"
                             viewBox="0 0 20 20"
                             xmlns="http://www.w3.org/2000/svg"
@@ -62,7 +67,7 @@ function Navbar({ setIsopen }: NavbarProps) {
                         </svg>
                     </div>
 
-                    <div className="hidden tab:block">
+                    <div className="hidden tab:block text-white">
                         {user !== null && (
                             <>
                                 <p>{`${user.first_name} ${user.last_name}`}</p>
@@ -81,8 +86,8 @@ function Navbar({ setIsopen }: NavbarProps) {
                             fill="none"
                             viewBox="0 0 24 24"
                             strokeWidth="1.5"
-                            stroke="#242254"
-                            className="w-8 h-8 text-white hover:cursor-pointer"
+                            stroke="white"
+                            className="w-8 h-8  hover:cursor-pointer"
                         >
                             <path
                                 strokeLinecap="round"
