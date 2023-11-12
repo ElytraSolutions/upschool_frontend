a86e0b4f7620928b40f319a47a504827d4ef99b5
Kaustuv Karki <karkikaustuv@gmail.com>
2023-11-12 16:47:17 +0545

    Global Reach css animation

--- src/pages/GlobalReach/GlobalReach.tsx
+++ src/pages/GlobalReach/GlobalReach.tsx
@@ -1,5 +1,6 @@
 import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
 import Map from './Map';
+import './AnimateStyles.css';
 import { RxCursorArrow } from 'react-icons/rx';
 
 const GlobalReach = () => {
@@ -23,24 +24,42 @@ const GlobalReach = () => {
                             <div className="absolute flex w-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 items-center justify-center">
                                 <div className="flex items-center justify-center flex-col  max-w-[800px] z-10">
                                     <h3
-                                        className="text-[20px] mb-[30px] font-lexend font-light tracking-[10px]"
+                                        className="text-[20px] mb-[30px] font-lexend font-light tracking-[8px]"
                                         style={{ color: '#FDCF60' }}
                                     >
                                         We are so proud that
                                     </h3>
                                     <h1
-                                        className="text-[28px] sm:text-[44px] md:text-[51px] text-center px-[1rem] font-lexend font-semibold mb-[1.5rem]"
+                                        className="text-[28px] sm:text-[44px] md:text-[51px] text-center px-[1.5rem] font-lexend font-semibold mb-[1.5rem]"
                                         style={{ color: 'white' }}
                                     >
                                         Students Over 170 Countries Are Learning
                                         With Upschool{' '}
                                     </h1>
-                                    <h2
-                                        className="text-[22px] md:text-[41px] sm:text-[34px] text-center mb-[1.7rem] font-lexend font-medium"
-                                        style={{ color: '#FDCF60' }}
-                                    >
-                                        Join The Movement
-                                    </h2>
+                                    <div className="flex flex-col items-center justify-center  mb-[1.7rem]">
+                                        <h2
+                                            className="text-[22px] md:text-[41px] sm:text-[34px] text-center font-lexend font-medium"
+                                            style={{ color: '#FDCF60' }}
+                                        >
+                                            Join The Movement
+                                        </h2>
+                                        <svg
+                                            className="-mt-3 animate-drawPath md:w-[25rem]  sm:w-[20rem] w-[13rem]"
+                                            width="400"
+                                            height="32"
+                                            viewBox="0 0 793 46"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                className="path stroke-[8]"
+                                                d="M6 30.7841C21.7883 30.7841 37.2527 30.1065 52.962 28.6204C62.4639 27.7216 72.0356 27.5841 81.5326 26.6043C100.558 24.6413 119.415 21.3537 138.477 19.7198C161.885 17.7134 185.439 17.0059 208.896 15.5399C223.888 14.6029 238.838 12.415 253.841 11.3109C265.953 10.4196 278.022 10.8234 290.133 10.229C308.824 9.31165 327.611 7.50908 346.29 6.93432C371.774 6.1502 397.315 7.36705 422.806 6.83597C462.957 5.9995 503.108 6 543.285 6C578.051 6 612.837 7.70037 647.536 9.98317C674.214 11.7383 701.011 13.7916 727.396 18.5888C730.85 19.2168 733.929 20.8802 737.083 22.3261C741.49 24.346 746.53 24.5308 751.147 25.9158C755.883 27.3366 759.497 28.7439 763.736 31.1284C765.431 32.0818 767.827 31.8197 769.686 32.5544C772.002 33.4701 774.061 34.7369 776.472 35.4066C779.061 36.1258 786.701 42.8755 786.701 37.8653"
+                                                stroke="#FDCF60"
+                                                stroke-width="8"
+                                                stroke-linecap="round"
+                                            />
+                                        </svg>
+                                    </div>
                                     <div
                                         className="px-[30px] py-[10px] border-2 border-white"
                                         style={{ color: 'white' }}
