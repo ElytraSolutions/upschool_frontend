c7ef54e927aa3054587b02fc6fb9879e337d0707
anuj_raymajhi <87628435+anuj-raymajhi@users.noreply.github.com>
2023-11-13 17:05:02 +0545

    Course Landing Page integration

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -3,36 +3,36 @@
 // import useUser from '../hooks/useUser';
 // import CharityProjectCard from './Charity/CharityProjectCard';
 // import CanvaPage from './Canva/CanvaPage';
-import CourseStaticUpschool from '../components/Course/CourseStaticUpschool';
-import CourseStaticVideo from '../components/Course/CourseStaticVideo';
-import CourseStaticTop from '../components/Course/CourseStaticTop';
-import CourseEnrol from '../components/Cards/Course/CourseEnrol';
-import CourseStaticBottom from '../components/Course/CourseStaticBottom';
-import CourseTestimonial from '../components/Course/CourseTestimonial';
+// import CourseStaticUpschool from '../components/Course/CourseStaticUpschool';
+// import CourseStaticVideo from '../components/Course/CourseStaticVideo';
+// import CourseStaticTop from '../components/Course/CourseStaticTop';
+// import CourseEnrol from '../components/Cards/Course/CourseEnrol';
+// import CourseStaticBottom from '../components/Course/CourseStaticBottom';
+// import CourseTestimonial from '../components/Course/CourseTestimonial';
 
-const commQuestions = [
-    {
-        question: 'Is the program really free?',
-        answer: "It's 100% free - yes. Nothing to pay to be involved.",
-    },
-    {
-        question: 'What ages is the program suitable for?',
-        answer: 'We have 5 year old children right through to adults participating. It’s differentiated and open to everyone.',
-    },
-    {
-        question:
-            'Do schools run this as a whole school project or can it be done as a class?',
-        answer: 'Either. Whole school engagement is the best way to have maximum impact, but it certainly can be done by one class or by one year level. You can even use it as an extension program for a small number of students too if that works for you.',
-    },
-    {
-        question: "I'm homeschooling my children. Can I participate?",
-        answer: 'Absolutely. Anyone can join!',
-    },
-    {
-        question: 'Do I need to be involved with a school to participate?',
-        answer: 'No, not at all. Individuals (adults and children) can go through the course.',
-    },
-];
+// const commQuestions = [
+//     {
+//         question: 'Is the program really free?',
+//         answer: "It's 100% free - yes. Nothing to pay to be involved.",
+//     },
+//     {
+//         question: 'What ages is the program suitable for?',
+//         answer: 'We have 5 year old children right through to adults participating. It’s differentiated and open to everyone.',
+//     },
+//     {
+//         question:
+//             'Do schools run this as a whole school project or can it be done as a class?',
+//         answer: 'Either. Whole school engagement is the best way to have maximum impact, but it certainly can be done by one class or by one year level. You can even use it as an extension program for a small number of students too if that works for you.',
+//     },
+//     {
+//         question: "I'm homeschooling my children. Can I participate?",
+//         answer: 'Absolutely. Anyone can join!',
+//     },
+//     {
+//         question: 'Do I need to be involved with a school to participate?',
+//         answer: 'No, not at all. Individuals (adults and children) can go through the course.',
+//     },
+// ];
 
 function Test() {
     // test string123
@@ -46,7 +46,7 @@ function Test() {
 
     return (
         <>
-            <div className="grid gap-y-10">
+            {/* <div className="grid gap-y-10">
                 <CourseStaticTop />
                 <div className="tab:hidden flex justify-center my-6 mx-6">
                     <CourseEnrol />
@@ -55,7 +55,7 @@ function Test() {
                 <CourseStaticUpschool />
                 <CourseTestimonial />
                 <CourseStaticBottom questionList={commQuestions} />
-            </div>
+            </div> */}
         </>
     );
 }
