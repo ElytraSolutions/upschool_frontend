09a2d4ac68ff19ba060aec730b04455db9b1c3a3
anuj_raymajhi <87628435+anuj-raymajhi@users.noreply.github.com>
2023-11-30 00:48:03 +0545

    Download button added Course Module

--- src/components/Course/CourseDescription.tsx
+++ src/components/Course/CourseDescription.tsx
@@ -1,6 +1,8 @@
 // import { FunctionalIFrameComponent } from './FuncIFrame';
 // import { useRef } from 'react';
 
+import { useEffect, useState } from 'react';
+
 function CourseDescription({ editorData, title, subtitle, theme }) {
     // const divRef = useRef<any>(null);
     // const [dynHeight, setDynHeight] = useState(0);
@@ -14,7 +16,31 @@ function CourseDescription({ editorData, title, subtitle, theme }) {
     //     }, 3000);
     //     return () => clearTimeout(timerId);
     // }, [divRef]);
+    const [showDownloadButton, setShowDownloadButton] = useState(false);
+    const [downloadHref, setDownloadHref] = useState('');
 
+    useEffect(() => {
+        //Check if a img tag occurs within a <a> tag... just an assumption...
+        const anchorWithImgRegex = /<a.*?href="(.*?)">.*?<img.*?>.*?<\/a>/i;
+
+        //check the occurence of the regex in the editorData
+        const contains = editorData.match(anchorWithImgRegex);
+
+        if (contains) {
+            const href = contains[1];
+            setDownloadHref(href);
+            setShowDownloadButton(true);
+        } else {
+            setShowDownloadButton(false);
+        }
+    }, [editorData]);
+    console.log('download button:   ', showDownloadButton);
+    const updatedEditorData = showDownloadButton
+        ? editorData.replace(
+              /(<a.*?href="(.*?)">.*?<img.*?>.*?<\/a>)/i,
+              `$1<p style="display:flex; justify-content:center; margin-left:20px; margin-right:20px"><button style="background-color:${theme}" class="course-download-button"><a href="${downloadHref}" download>download your free teaching program here</a></button></p>`,
+          )
+        : editorData;
     return (
         <>
             <div className="grid w-full font-sans text-theme-color">
@@ -31,14 +57,14 @@ function CourseDescription({ editorData, title, subtitle, theme }) {
                     className={`flex w-full justify-center overflow-y-hidden`}
                     // style={{ height: `${dynHeight}px` }}
                 >
-                    <div className="flex xl:w-[1120px] lg:w-[1020px] w-screen font-kumbh">
+                    <div className="flex xl:w-[1120px] lg:w-[1020px] w-screen font-kumbh text-theme-color">
                         {/* <FunctionalIFrameComponent title={title}> */}
                         <div
                             // ref={divRef}
                             style={{ display: 'grid', gap: '1.1rem' }}
                             dangerouslySetInnerHTML={{
-                                __html: editorData
-                                    ? editorData
+                                __html: updatedEditorData
+                                    ? updatedEditorData
                                           .replace(
                                               /<iframe/g,
                                               '<iframe style="width:100%"',
