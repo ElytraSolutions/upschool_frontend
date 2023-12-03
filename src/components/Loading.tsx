074aff5ff169b5df22442b9aa49f474c089e1e60
anuj_raymajhi <87628435+anuj-raymajhi@users.noreply.github.com>
2023-12-03 15:27:09 +0545

    Loading titles and Lesson section Sidebar loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,10 @@
+import { useEffect } from 'react';
 import Loader from './Loader/Loader';
 
 export default function Loading() {
+    useEffect(() => {
+        document.title = 'Loading | Upschool';
+    }, []);
     return (
         <div className="md:-mt-24 -mt-16 h-screen flex w-full items-center justify-center">
             <Loader />
