cf546f8258f3d38ed985899eb217b521fcbfbeb3
Kaustuv Karki <karkikaustuv@gmail.com>
2023-12-04 19:08:15 +0545

    Capitalize

--- src/components/Cards/Course/CourseEnrol.tsx
+++ src/components/Cards/Course/CourseEnrol.tsx
@@ -116,7 +116,7 @@ function CourseEnrol({ thumbnail }) {
                         {enrollmentStatus.enrolled
                             ? checkAllLessonCompleted()
                                 ? 'Revisit Course'
-                                : 'Continue to Course'
+                                : 'Continue To Course'
                             : 'Enrol Now'}
                     </button>
                     {enrollmentStatus.enrolled && checkAllLessonCompleted() && (
