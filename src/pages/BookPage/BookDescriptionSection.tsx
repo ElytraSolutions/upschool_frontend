4a413e6cb9add94500f44bc4b0d441d6983862c4
Kaustuv Karki <karkikaustuv@gmail.com>
2023-12-06 19:54:11 +0545

    Download Icon Updated && book page changes

--- src/pages/BookPage/BookDescriptionSection.tsx
+++ src/pages/BookPage/BookDescriptionSection.tsx
@@ -34,13 +34,13 @@ const BookDescriptionSection = () => {
             <div className="flex flex-col md:grid md:grid-cols-7 lg:grid-cols-6  md:grid-rows-4  xl:grid-rows-5 gap-2 text-blue-950 h-full w-full">
                 <div className="md:col-span-4 md:row-span-2 h-full w-full ">
                     <div className="text-md sm:text-xl">{bookDescription}</div>
-                    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 sm:m-4 font-bold flex-wrap">
+                    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 my-4 font-bold flex-wrap">
                         <button
                             onClick={openPopup}
                             className="bg-red-upschool hover:bg-pink-900 text-white py-2 px-4 my-1 font-normal text-left sm:text-sm text-xs"
                         >
                             BUY PAPERBACK
-                            <span className="block text-left font-bold">
+                            <span className="block text-left font-regular text-[1.1rem]">
                                 AU $59.0
                             </span>
                         </button>
@@ -49,7 +49,7 @@ const BookDescriptionSection = () => {
                             className="bg-red-upschool text-left hover:bg-pink-900 text-white py-2 px-4 my-1 font-normal sm:text-sm text-xs"
                         >
                             BUY HARDBACK
-                            <span className="block text-left font-bold">
+                            <span className="block text-left font-regular text-[1.1rem]">
                                 AU $49.0
                             </span>
                         </button>
@@ -58,7 +58,7 @@ const BookDescriptionSection = () => {
                             className="bg-red-upschool hover:bg-pink-900 text-white py-2 px-4 my-1 font-normal text-left sm:text-sm text-xs"
                         >
                             BUY EBOOK
-                            <span className="block text-left font-bold">
+                            <span className="block text-left font-regular text-[1.1rem]">
                                 AU $5.0
                             </span>
                         </button>
@@ -66,7 +66,25 @@ const BookDescriptionSection = () => {
                             <Link to="/report-a-problem">REPORT ISSUE</Link>
                         </button>
                     </div>
-                    <div className="text-gray-500 text-sm">What to expect?</div>
+                    <div className="flex gap-2 items-center">
+                        <svg
+                            width="18"
+                            height="18"
+                            viewBox="0 0 18 18"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M9 0C4.02979 0 0 4.03124 0 9C0 13.9717 4.02979 18 9 18C13.9702 18 18 13.9717 18 9C18 4.03124 13.9702 0 9 0ZM9 3.99194C9.84179 3.99194 10.5242 4.67434 10.5242 5.51613C10.5242 6.35792 9.84179 7.04032 9 7.04032C8.15821 7.04032 7.47581 6.35792 7.47581 5.51613C7.47581 4.67434 8.15821 3.99194 9 3.99194ZM11.0323 13.2097C11.0323 13.4502 10.8373 13.6452 10.5968 13.6452H7.40323C7.16273 13.6452 6.96774 13.4502 6.96774 13.2097V12.3387C6.96774 12.0982 7.16273 11.9032 7.40323 11.9032H7.83871V9.58064H7.40323C7.16273 9.58064 6.96774 9.38566 6.96774 9.14516V8.27419C6.96774 8.0337 7.16273 7.83871 7.40323 7.83871H9.72581C9.9663 7.83871 10.1613 8.0337 10.1613 8.27419V11.9032H10.5968C10.8373 11.9032 11.0323 12.0982 11.0323 12.3387V13.2097Z"
+                                fill="black"
+                                fill-opacity="0.3"
+                            />
+                        </svg>
+
+                        <div className="text-gray-500 text-base underline font-nunito ">
+                            What to expect?
+                        </div>
+                    </div>
                 </div>
                 <div className="md:col-span-3 lg:col-span-2 md:row-span-4 xl:row-span-5 h-full w-full ">
                     <AuthorsRecommendedProject />
