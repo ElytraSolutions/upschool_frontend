3880638484feb27a1e560d30948ad8a6338a5697
bikrantbdr <bikrantbdr@gmail.com>
2023-12-06 20:58:23 +0545

    lesson section sidebar height fixed

--- src/parts/PartsLesson/Sidebar.tsx
+++ src/parts/PartsLesson/Sidebar.tsx
@@ -11,7 +11,7 @@ export default function Sidebar({ courseSlug, chapters }: SidebarProps) {
     return (
         <>
             <div className="flex flex-row justify-center items-center bg-[#b81342] w-full mt-[0.4px] ">
-                <div className="flex flex-row items-center justify-center text-white w-fit gap-1  h-14">
+                <div className="flex flex-row items-center justify-center text-white w-fit gap-1  h-[70px]">
                     <span className=" ">
                         <img
                             src={resolveImgURL('/images/OpenBook.png')}
